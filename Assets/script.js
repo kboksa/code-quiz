9fb3c2a953f82de9da58cb5a3031c04874c068a1
kalina <kalina.boksa@gmail.com>
2022-08-07 15:57:59 -0500

    add section to HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,64 @@
 // STEP 1: Create a start button the user can click and begin the game
+var welcom = document.querySelector("#introduction");
 var start = document.querySelector("#start-button");
-// STEP 2: Once the user clicks the start button they are prompted with a questions
-// STEP 2 (A):  As soon as the user starts the quiz the timer starts to run down
-var timeLeft =document.getElementById("timer");
-var secondsLeft = 60;
-var questionNumer = 0;
-var totalScore = 0;
-var questionsCount =1;
-function countdown()[
-    var timerInterval = setInterval(function (){
-        secondLeft--;
-        timeLeft.textContent = "Time left: " + secondsLeft + "s";
+var into = document.querySelector("#intro");
 
-            if (secondLeft <= 0){
-                clearInterval(timerInterval);
-                timerLeft.textContent = "Time is up!";
-                finish.textContent = "Time is up!";
-                gameOver();
-            } else if (questionCount >= questionSource.length +1){
-                clearInterval(timerInterval);
-                gameOver();
-            }
-    }, 1000);
-]
+function startQuiz() {
+  console.log(start);
+}
+
+// STEP 2: Once the user clicks the start button they are prompted with a questions
+// var questionSource[
+//     {
+//         question: "Question 1: Javascript is an _______ language?",
+//         chocies: [ "a. Object-Oriented", "b. Object-Based", "c. Procedural", "d. None of the above"],
+//         answer: "a"
+//     },
+//     {
+//         question: "Question 2: Which of the following methods can be used to display data in some form using Javascript?",
+//         chocies: ["a. document.write()", "b. console.log()", "c. window.alert()", "d. All of the above"],
+//         answer: "d"
+//     },
+//     {
+//         question: "Question 3: When an operators value is NULL, the typeof returned by the unary operator is: ",
+//         chocies: ["a. Boolean", "b. Undefined", "c. Object", "d. Integer"]
+//         answer: "c"
+//     },
+//     {
+//         question: "Question 4: Which function is used to serialize an object into a JSON string in Javascript? ",
+//         chocies: ["a. stringify()", "b. parse()", "c. convert()", "d. None of the above"],
+//         answer: "a"
+//     },
+//     {
+//         question: "Question 4: How to stop an interval timer in Javascript?",
+//         chocies: ["a. clearTimer", "b. intervalOver", "c. clearInterval", "d. None of the above"],
+//         answer: "c"
+//     },
+
+// ]
+
+// STEP 2 (A):  As soon as the user starts the quiz the timer starts to run down
+// var timeLeft =document.getElementById("timer");
+// var secondsLeft = 60;
+// var questionNumer = 0;
+// var totalScore = 0;
+// var questionsCount =1;
+// function countdown()[
+//     var timerInterval = setInterval(function (){
+//         secondLeft--;
+//         timeLeft.textContent = "Time left: " + secondsLeft + "s";
+
+//             if (secondLeft <= 0){
+//                 clearInterval(timerInterval);
+//                 timerLeft.textContent = "Time is up!";
+//                 finish.textContent = "Time is up!";
+//                 gameOver();
+//             } else if (questionCount >= questionSource.length +1){
+//                 clearInterval(timerInterval);
+//                 gameOver();
+//             }
+//     }, 1000);
+// ]
 // STEP 3: The user is told whether they get the answer right or wrong
 // STEP3(A):    The user is able to click to the next question when they answer the question
 
